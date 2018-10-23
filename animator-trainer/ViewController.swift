4d059a2106f5132a04366db53cabc3e26d550778
Derik Flanary <derikflanary@gmail.com>
2018-10-23 12:38:31 -0600

    Initial Commit

---
+++ animator-trainer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animator-trainer
+//
+//  Created by Derik Flanary on 10/23/18.
+//  Copyright © 2018 Derik Flanary. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
